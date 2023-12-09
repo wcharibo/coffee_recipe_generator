ece460b3b6e84671b50e70f4056c495d7607dca0
wcharibo <woonggi4283@gmail.com>
2023-12-09 23:17:23 +0900

    Add feature that slice audio file per recipe

--- main.py
+++ main.py
@@ -3,14 +3,30 @@ import sys
 import os
 import text_extraction
 from pydub import AudioSegment
+import fnmatch
 
-def get_yt_text(link):
-    text_list = []
+def find_files(directory, pattern):
+    files = []
+    for root, dirnames, filenames in os.walk(directory):
+        for filename in fnmatch.filter(filenames, pattern):
+            files.append(os.path.join(root, filename))
 
+    return files
+
+def get_yt_video(link):
     yt = YouTube(link)
     mp4_file_path = yt.streams.filter(only_audio=True).first().download()
     file_path = mp4_file_path.replace('mp4','mp3')
     os.rename(mp4_file_path, file_path)
+    return file_path
+
+def get_yt_text(link):
+    text_list = []
+
+    if os.path.exists(link):
+        file_path = link
+    else:
+        file_path = get_yt_video(link)
 
     try:
         audio = AudioSegment.from_file(file_path, "mp3")
@@ -41,12 +57,22 @@ def save_text_to_file(text_list, base_filename="test"):
     return filename
 
 if __name__ =="__main__":
-    if len(sys.argv) !=2:
-        print("Usage: python main.py <youtube_link>")
-        sys.exit(1)
+    directory = "/home/woong/coffee_recipe_generator/"
+    pattern = "output_*.mp3"
 
-    youtube_link = sys.argv[1]
-    text = get_yt_text(youtube_link)
-    result_filename = save_text_to_file(text)
+    matching_files = find_files(directory, pattern)
+
+    if matching_files:
+        for file in matching_files:
+            text = get_yt_text(file)
+            result_filename = save_text_to_file(text)
+    else:
+        if len(sys.argv) !=2:
+            print("Usage: python main.py <youtube_link or file_path>")
+            sys.exit(1)
+        youtube_link = sys.argv[1]
+        text = get_yt_text(youtube_link)
+        result_filename = save_text_to_file(text)
+
     print("result:")
     print(text)
